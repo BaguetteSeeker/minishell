e33056030143b1065d433ec6a73300ff0e485fc7
mouaret <mouaret@ouaret.com>
2025-01-17 23:54:02 +0100

    Added my version of [struct tokens]

---
+++ struct_tokens_so.h
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   struct_tokens_so.h                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: souaret <souaret@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/17 22:04:36 by souaret           #+#    #+#             */
+/*   Updated: 2025/01/17 23:52:41 by souaret          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef STRUCT_TOKENS_SO_H
+# define STRUCT_TOKENS_SO_H
+
+/***************************************************************************
+ * 
+ * type : type of the token [enum type]
+ * str : string of the token, with no leading or trailing spaces
+ * args : array of strings, 
+ *      each string is an argument (or option) of the token
+ * next : pointer to the next token in the list
+ * prev : pointer to the previous token in the list
+ *  TODO: prev not managed so far.
+ * 
+ ***************************************************************************/
+typedef struct s_token
+{
+    int type;
+    char *str;
+    char **args;
+    struct s_token *next;
+} t_token;
+
+#endif

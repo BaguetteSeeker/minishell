5d9367a1b9034756eeffd1a5626638b3a944b9df
epinaud <enzo.pinaud+42@protonmail.com>
2025-01-24 10:15:56 +0100

    Introduced minishell header with includes readline/signal/libft

---
+++ minishell.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/15 18:31:31 by epinaud           #+#    #+#             */
+/*   Updated: 2025/01/23 19:07:57 by epinaud          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# define _DEFAULT_SOURCE
+# define LL_TYP /* Remplacer ce commentaire par le nom de la structure chainée voulue au sein du projet*/
+//Définir impérativement la struct chainée avant l'include libft qui en fera usage
+# include <libft.h>
+# include <signal.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+
+#endif

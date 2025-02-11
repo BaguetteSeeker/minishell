17fd9e2de7d97e7b41e4693c5112ae56a0b5238b
Enzo Pinaud <epinaud@bess-f2r5s2.clusters.42paris.fr>
2025-02-11 16:16:59 +0100

    Clean main branch

--- minishell.h
+++ minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: souaret <souaret@student.42.fr>            +#+  +:+       +#+        */
+/*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 18:31:31 by epinaud           #+#    #+#             */
-/*   Updated: 2025/01/25 16:30:59 by souaret          ###   ########.fr       */
+/*   Updated: 2025/02/11 16:38:48 by epinaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,12 +22,4 @@
 # include <unistd.h>
 # include <readline/readline.h>
 # include <readline/history.h>
-
-
-# include "struct_cmds.h"
-# include "system_utils.h"
-# include "cmd_errors.h"
-# include "cmd_utils.h"
-# include "cmd_exec.h"
-
 #endif

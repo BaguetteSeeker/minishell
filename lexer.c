8fe1d7f6311394ff78f6def46957838816a43491
epinaud <enzo.pinaud+42@protonmail.com>
2025-02-11 16:48:55 +0100

    Pending lexer

--- lexer.c
+++ lexer.c
@@ -6,12 +6,21 @@
 /*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/27 14:13:42 by epinaud           #+#    #+#             */
-/*   Updated: 2025/01/29 19:17:57 by epinaud          ###   ########.fr       */
+/*   Updated: 2025/02/05 12:24:00 by epinaud          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+int	strlentill(char *str, char limit)
+{
+	int	count;
+
+	count = 0;
+	while (str[count] && str[count] != limit)
+		count++;
+	return (count);
+}
 
 //Rooting function that passes a pos index of the input to subfunctions that will fill a t_token struct
 
@@ -19,6 +28,15 @@ t_token	*lexer(char *prompt)
 {
 	t_token	*token;
 	(void)prompt;
+	int	i;
+	
+	char	**items;
+
+	items = ft_split(prompt, ' ');
+	while (items)
+	{
+		items++;
+	}
 
 	token = NULL;
 	return (token);

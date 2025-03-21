1d52360d077d1295292fe607e35a81fe9e055458
Enzo Pinaud <epinaud@bess-f4r1s14.clusters.42paris.fr>
2025-03-21 17:07:07 +0100

    Introduced struct definitions for AST table;

---
+++ cmd_table.h
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cmd_table.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: epinaud <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/21 12:28:11 by epinaud           #+#    #+#             */
+/*   Updated: 2025/03/21 14:48:36 by epinaud          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef	CMD_TABLE_H
+# define CMD_TABLE_H
+# include <stddef.h>
+
+enum    e_node_types
+{
+    CMD,
+    ANDIF,
+    ORIF
+};
+
+typedef struct  s_redirs
+{
+   size_t  infile;
+   size_t  outfile;
+}  t_redirs;
+
+typedef struct  s_command
+{
+    int       type;
+    void      *prev;
+    char      *cmd;
+    char      **args;
+    t_redirs  io_streams;
+} t_command;
+
+typedef struct s_if_node
+{
+    int     type;
+    void    *prev;
+    void    *next_a;
+    void    *next_b;
+}    t_if_node;
+#endif

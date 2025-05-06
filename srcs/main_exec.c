f60d7d016932ebf1c5d0dcfe360261d0b9abeb49
Sidi Hamed OUARET <mouaret@ouaret.com>
2025-05-06 18:48:39 +0200

    EXEC updated : pipe execution tentative 2 -WIP

--- srcs/main_exec.c
+++ srcs/main_exec.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main_exec.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: souaret <souaret@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 15:11:46 by souaret           #+#    #+#             */
-/*   Updated: 2025/05/06 15:01:35 by marvin           ###   ########.fr       */
+/*   Updated: 2025/05/06 17:01:56 by souaret          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -57,24 +57,25 @@ int	main2(int argc, char *argv[], char *env[])
 		builtin_init();
 		// create AST for "ls | wc"
 		cmd_ls = node_new("/bin/ls");
-		cmd_ls->cmd_args = (char *[]){"ls", NULL};
+		// cmd_ls->cmd_args = (char *[]){"ls", NULL};
 
 		cmd_wc = node_new("/bin/wc");
-		cmd_wc->cmd_args = (char *[]){"wc", NULL};
+		//cmd_wc->cmd_args = (char *[]){"wc", NULL};
 
 		cmd = node_new("|");
+		cmd->node_type = N_OPE_PIPE;
 		cmd->left = cmd_ls;
 		cmd->right = cmd_wc;
 		cmd->node_type = N_OPE_PIPE;
-		cmd->cmd_str = ft_strdup("|");
+		//cmd->cmd_str = ft_strdup("|");
 		//cmd->cmd_id = CMD_PIPE;
 		cmd->is_child = false;
 		cmd->is_builtin = false;
 		root = cmd; 	//node_add_top(NULL, cmd_ls);
 
-		cmd = node_new(argv[1]);
-		cmd->cmd_args = argv + 1; // "/bin/ls -l"
-		root = node_add_top(NULL, cmd);
+		// cmd = node_new(argv[1]);
+		// cmd->cmd_args = argv + 1; // "/bin/ls -l"
+		//root = node_add_top(NULL, cmd);
 		cmd_set(root);
 		cmd_exec();
 	}
